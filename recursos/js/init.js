940d5145b6316c66bf01f501bcd7f6a2b328b93c
Jonathan <linkvilory@hotmail.com>
2015-03-09 11:30:32 -0600

    Nueva revisión y arreglo de código e imagenes
    Se quitaron los recursos que no se están utilizando

--- recursos/js/init.js
+++ recursos/js/init.js
@@ -9,6 +9,24 @@ $(function(){
 	$("#showYoutubePlayer").click(function (){
 		$(".container-fluid.fullscreen").show();
 	});
+	$("#showYoutubePlayer").hover(function (){
+		$("#showYoutubePlayer video").show();
+		$("#showYoutubePlayer video").get(0).play();
+		$(".containerDefault .trailer").css({
+			"background": "#000",
+			"opacity": "1"
+		});
+
+	}, function(){
+		$("#showYoutubePlayer video").hide();
+		$('#showYoutubePlayer video').get(0).pause();
+		$(".containerDefault .trailer").css({
+			"background": "",
+			"background-image": "url(recursos/css/images/bg/trailer.jpg)",
+			"background-size": "cover",
+			"opacity": "0.5"
+		});
+	});
 	$("#explorar").click(function(){
 		window.location = "./encuesta.html";
 	});
