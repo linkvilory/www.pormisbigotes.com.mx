80a8b6e5c556cbe268faf6cfcd1884eb95b6f043
Jonathan <linkvilory@hotmail.com>
2015-03-18 12:30:24 -0600

    Liga demo
    Esta es la ultima modificación del sitio, en estos momentos esta arriba
    la liga demo

--- recursos/js/main.js
+++ recursos/js/main.js
@@ -26,7 +26,7 @@ $(function(){
 		window.open('https://www.facebook.com/porbigotes?ref=hl','_blank');
 	});
 	$("#facebook-s").click(function(){
-		window.open('http://www.facebook.com/sharer.php?s=100&p[title]=Por%20mis%20bigotes&p[summary]=No%20te%20pierdas%20la%20producci%C3%B3n%20m%C3%A1s%20bigotuda%20del%20cine.%20%C2%A1Pronto%20en%20cines!&p[url]=http://pormisbigotes.com.mx&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT','_blank');
+		window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fpormisbigotes.com.mx','_blank');
 	});
 	$("#twitter-l").click(function(){
 		window.open('https://twitter.com/PorBigotes?lang=es','_blank');
